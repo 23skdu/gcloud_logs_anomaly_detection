c6f49a28c405a3a678cda2e278df99e909f794e8
rdmochowski <rdmochowski@apple.com>
2025-05-20 16:02:57 -0700

    adding seaborn for viz

--- gcloud_logs_detect.py
+++ gcloud_logs_detect.py
@@ -5,7 +5,10 @@ from google.cloud import logging
 from sklearn.ensemble import IsolationForest
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+severity_mapping = { 'DEBUG': 1, 'INFO': 2, 'WARNING': 3, 'ERROR': 4, 'CRITICAL': 5 }
 logname = os.getenv('LOG_NAME', 'loremipsumevents')
 df = pd.DataFrame()
 
@@ -20,3 +23,15 @@ logger = client.logger(logname)
 entries = logger.list_entries()
 df = pd.DataFrame(entries)
 df['timestamp'] = df['timestamp'].astype('int64')
+df['severity'] = df['severity'].map(severity_mapping).fillna(0)
+
+X = df[['timestamp','severity']]
+scaler = StandardScaler()
+Z = scaler.fit_transform(X)
+Z_train, Z_test = train_test_split(X, test_size=0.2, random_state=42)
+model = IsolationForest(n_estimators=100, contamination='auto', random_state=42)
+model.fit(Z_train)
+y_pred = model.predict(Z_test)
+
+anomaly_indices = [i for i, pred in enumerate(y_pred) if pred == -1]
+print("Anomaly Indices:", anomaly_indices)
