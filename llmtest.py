f16bdcd5a80437958dd4ed33e9831386bafae949
rdmochowski <rdmochowski@apple.com>
2025-06-13 21:19:12 -0700

    change to reading modelname from env var

--- llmtest.py
+++ llmtest.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
-import argparse
+import os,argparse
 from langchain_ollama import OllamaLLM
 from langchain.prompts import PromptTemplate
 from langchain.chains import create_extraction_chain
 from langchain_core.runnables import chain
+modelname = os.environ.get("MODELNAME","smollm2:135m")
 parser = argparse.ArgumentParser(description="ask a question to a local Ollama LLM.")
 parser.add_argument("question")
 args = parser.parse_args()
 question = (args.question)
 template = """Question: {question} """
 prompt = PromptTemplate(template=template, input_variables=["question"])
-llm = OllamaLLM(model="huihui_ai/nemotron-v1-abliterated:8b")
+llm = OllamaLLM(model=modelname)
 runnable = prompt | llm
 response = runnable.invoke({"question": question}) 
 print(response)
